ff7494e2b40bf135cb2cd8330ec9f6b64a0da6c1
Jay Sobel <jsobel@theadkgroup.com>
2016-11-20 21:38:09 -0500

    Initial Files

---
+++ florida.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# getting the input CSV
+florida = pd.read_csv("clean_florida.csv", header=0)
+
+x = florida["dem"].tolist()
+x = [int(i) for i in x]
+y = florida["rep"].tolist()
+y = [int(i) for i in y]
+
+colors = florida["digital"].tolist()
+
+for i in range(0, len(colors)):
+    if colors[i] == 1:
+        colors[i] = "orange"
+    else:
+        colors[i] = "green"
+
+plt.scatter(x, y, s=60, c=colors)
+plt.grid(True, which='both')
+plt.xlabel("Democrat Vote Change")
+plt.ylabel("Republican Vote Change")
